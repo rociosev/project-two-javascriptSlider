3a3231cc748cad55218eb7abdac59d4f4bf94a6c
Rocio Sevilla <rociosev@live.unc.edu>
2015-02-25 23:14:32 -0500

    Adding files
    Adding files for the project

---
+++ scripts/Qtip.js
@@ -0,0 +1,8 @@
+// Create the tooltips only when document ready
+$(document).ready(function () {
+    
+    // This will automatically grab the 'title' attribute and replace
+    // the regular browser tooltips for all <a> elements with a title attribute!
+    $('.linktest').qtip();
+    
+});
